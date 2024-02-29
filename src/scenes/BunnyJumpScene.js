e43dcb8069a1ff7e8739dbc7a7a31dd243b1766b
Glo <142151062+glointhedarkv@users.noreply.github.com>
2024-02-29 14:15:32 +0700

    first commit

--- src/scenes/BunnyJumpScene.js
+++ src/scenes/BunnyJumpScene.js
@@ -1,5 +1,7 @@
 import Phaser from "phaser"
 
+var platforms, player
+
 export default class BunnyJumpScene extends Phaser.Scene {
     constructor() {
       super('bunny-jump-scene')
@@ -15,8 +17,31 @@ export default class BunnyJumpScene extends Phaser.Scene {
   
     create() {
       this.add.image(240, 320, 'background')
-      this.add.image(240, 320, 'platform')
+
+      this.platforms = this.physics.add.staticGroup()
+      for (let i = 0; i < 5; i++) {
+        const x = Phaser.Math.Between(80, 400)
+        const y = 150 * i
+
+        const platformChild = this.platforms.create(x, y, 'platform')
+        platformChild.setScale(0.5)
+        platformChild.refreshBody()
+        const body = platformChild.body
+        body.updateFromGameObject()
+      }
+
+      this.player = this.physics.add.sprite(240, 320, 'bunny_stand')
+      .setScale(0.5)
+      this.physics.add.collider(this.platforms, this.player)
+
     }
-  
-    update() {}
+
+    update() {
+      const touchingDown = this.player.body.touching.down
+
+      if (touchingDown) {
+        this.player.setVelocity(-300)
+        this.player.setTexture('bunny_jump')
+      }
+    }
   }
